e4825f20f7827771d7fd1da18921b81c3357d21c
Amelia Galgon <agalgon@gmail.com>
2018-01-08 16:09:07 -0500

    create a create_submission method. first attempt

--- lib/kele.rb
+++ lib/kele.rb
@@ -43,4 +43,10 @@ class Kele
     end
   end
 
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment = "")
+    response = get_me
+    enrollment_id = response['id']
+    response = self.class.post("#{@base_uri}/checkpoint_submissions", body: { "assignment_branch": assignment_branch, "assignment_commit_link": assignment_commit_link, "checkpoint_id": checkpoint_id, "comment": comment, "enrollment_id": enrollment_id })
+  end
+
 end
